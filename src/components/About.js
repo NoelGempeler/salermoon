9b494068e7610c03b6fc25639cce9212e3d4c865
Noel Gempeler <noel.gempeler@gmail.com>
2020-05-10 15:56:15 +0200

    shop sachen gemacht

--- src/components/About.js
+++ src/components/About.js
@@ -7,34 +7,68 @@ import bild_4 from "./../images/Travis-Scott-The-Scotts-World-Tie-Dye-T-Shirt-Ti
 const About = () => {
   return (
     <div>
-      <h2 className="mt-10 flex justify-center underline text-3xl ">
+      <h2 className="mt-12 flex justify-center border-b border-w pb-12 text-3xl ">
         Our collection
       </h2>
       <div className=" flex justify-around pt-24 ml-32  mr-32 mb-20">
         <div>
-          <img src={bild_1} className="max-w-xs" alt="" />
+          <div>
+            <img src={bild_1} className="max-w-xs" alt="" />
+          </div>
+          <div>
+            <h2 className="ml-14 mt-5 font-bold p-6">
+              Travis Scott Fortnite Action Figure Duo Set 12”
+            </h2>
+            <h3 className="ml-14 pl-8">
+              {" "}
+              Price 420-.
+              <br /> Condition: New
+            </h3>
+          </div>
         </div>
         <div>
-          <h2></h2>
+          <div>
+            <img src={bild_2} className="max-w-xs" alt="" />
+          </div>
+          <div>
+            <h2 className="ml-14 mt-5 font-bold p-6">
+              Travis Scott Fortnite Action Figure Duo Set 12”
+            </h2>
+            <h3 className="ml-14 pl-8">
+              {" "}
+              Price 130-.
+              <br /> Condition: New
+            </h3>
+          </div>
         </div>
         <div>
-          <img src={bild_2} className="max-w-xs" alt="" />
-        </div>
-        <div>
-          <h2></h2>
-        </div>
-        <div>
-          <img src={bild_3} className="max-w-xs" alt="" />
-        </div>
-        <div>
-          <h2></h2>
+          <div>
+            <img src={bild_3} className="max-w-xs" alt="" />
+          </div>
+          <div>
+            <h2 className="ml-14 mt-5 font-bold p-6">
+              Travis Scott Fortnite Action Figure Duo Set 12”
+            </h2>
+            <h3 className="ml-14 pl-8">
+              {" "}
+              Price 150-.
+              <br /> Condition: New
+            </h3>
+          </div>
         </div>
         <div>
           <div>
             <img src={bild_4} className="max-w-xs" alt="" />
           </div>
           <div>
-            <h2>HALLO</h2>
+            <h2 className="ml-14 mt-5 font-bold p-6">
+              Travis Scott Fortnite Action Figure Duo Set 12”
+            </h2>
+            <h3 className="ml-14 pl-8">
+              {" "}
+              Price 125-.
+              <br /> Condition: New
+            </h3>
           </div>
         </div>
       </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,14 +3,17 @@ import "./Navigation.css";
 
 const Navigation = () => {
   return (
-    <div className="fixed top-0 left-0 z-50 text-white w-full p-5 ">
+    <div className="absolute top-0 left-0 z-50 text-white w-full p-5 ">
       <div className="flex justify-between items-center textcolor">
         <div className="text-3xl ml-24"> Salermoon </div>
         <div className="mr-24" style={{ display: "flex", marginRight: "70px" }}>
-          <div className="border-b-2 border-transparent  transition ease-in duration-700 mr-16 cursor-pointer hover:border-renovering-rot">
-            About
+          <div
+            onClick={handleClick}
+            className="border-b-2 border-transparent  transition ease-in duration-700 mr-16 cursor-pointer hover:border-renovering-rot"
+          >
+            Shop
           </div>
-          <div className="mr-16 cursor-pointer">Shop</div>
+          <div className="mr-16 cursor-pointer"></div>
           <div className="cursor-pointer">Contact</div>
         </div>
       </div>
